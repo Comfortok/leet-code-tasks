5b412bb0c85217c809f5ec0f78b293c507b471de
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-01-27 15:31:26 +0600

    Time: 19 ms (46.54%), Space: 40.8 MB (26.02%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,35 +5,23 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start = 1, end = n;
         while (start < end) {
-            int mid = start + (end-start) / 2;
-            if (!isBadVersion(mid)) {
-                start = mid + 1;
+            if (end - start < 2) {
+                if (isBadVersion(start)) {
+                    return start;
+                } else {
+                    return end;
+                }
+            }
+            
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
             } else {
-                end = mid;            
+                start = mid + 1;  
             }
         }        
         
         return start;
-        // int mid = n / 2;
-        // return getFirstBadVersion(mid, n);
     }
     
-    public int getFirstBadVersion(int mid, int n) {
-        if (n - mid < 2) {
-            if (isBadVersion(mid)) {
-                return mid;
-            } else {
-                return n;
-            }
-        }
-        
-        int result;
-        if (isBadVersion(mid)) {
-            result = getFirstBadVersion(mid / 2, mid);
-        } else {
-            result = getFirstBadVersion(mid + ((n - mid) / 2), n);
-        }
-        
-        return result;
-    }
 }
