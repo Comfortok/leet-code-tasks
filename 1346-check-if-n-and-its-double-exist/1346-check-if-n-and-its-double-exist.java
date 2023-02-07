29494d2dfa804120b4db81eb4aac4b135b09debb
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-07 17:07:18 +0600

    Time: 1 ms (99.62%), Space: 41.5 MB (97.69%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -3,12 +3,13 @@ class Solution {
         if (arr.length == 2 && (arr[0] == arr[1] * 2 || arr[1] == arr[0] * 2)) {
             return true;
         }
-            
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j] * 2 || arr[j] == arr[i] * 2) {
-                    return true;
-                }
+        
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (set.contains(i * 2) || (i % 2 == 0 && set.contains(i/2))) {
+                return true;
+            } else {
+                set.add(i);
             }
         }
             
