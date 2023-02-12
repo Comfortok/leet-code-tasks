c167484449c98f9685efc20eea3a771f9ef2a04a
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-12 16:23:00 +0600

    Time: 995 ms (5.15%), Space: 44.4 MB (72.10%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.java
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.java
@@ -1,26 +1,17 @@
 class Solution {
     public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
         int[][] coords = new int[rows * cols][];
+        coords[0] = new int[]{rCenter, cCenter};
 
-        int count = 0;
+        int count = 1;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                coords[count] = new int[]{i, j};
-                count++;
+                if (coords[0][0] != i || coords[0][1] != j) {
+                    coords[count] = new int[]{i, j};
+                    count++;
+                }
             }
         }
-
-        int currentIndex = -1;
-        for (int i = 0; i < coords.length; i++) {
-            if (coords[i][0] == rCenter && coords[i][1] == cCenter) {
-                currentIndex = i;
-            }
-        }
-
-        coords[0][0] = rCenter;
-        coords[0][1] = cCenter;
-        coords[currentIndex][0] = 0;
-        coords[currentIndex][1] = 0;
 
         for (int i = 1; i < coords.length - 1; i++) {
             for (int j = i + 1; j < coords.length; j++) {
