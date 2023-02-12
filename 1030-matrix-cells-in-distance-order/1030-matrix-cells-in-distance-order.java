f707a497ca9123eaefeee6ead90f72ec0f729ade
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-12 16:53:22 +0600

    Time: 14 ms (62.66%), Space: 44.4 MB (61.37%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.java
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.java
@@ -13,17 +13,8 @@ class Solution {
             }
         }
 
-        for (int i = 1; i < coords.length - 1; i++) {
-            for (int j = i + 1; j < coords.length; j++) {
-                int distance1 = Math.abs(rCenter - coords[i][0]) + Math.abs(cCenter - coords[i][1]);
-                int distance2 = Math.abs(rCenter - coords[j][0]) + Math.abs(cCenter - coords[j][1]);
-                if (distance2 < distance1) {
-                    int[] temp = coords[i];
-                    coords[i] = coords[j];
-                    coords[j] = temp;
-                }
-            }
-        }
+        Arrays.sort(coords, (a, b) -> Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter)
+                - Math.abs(b[0] - rCenter) - Math.abs(b[1] - cCenter));
 
         return coords;
     }
