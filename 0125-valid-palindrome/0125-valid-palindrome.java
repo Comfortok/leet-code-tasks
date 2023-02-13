a1f89e3d2bbe0527393e3f779515be8f51c2f6bb
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-13 12:27:54 +0600

    Time: 48 ms (42.92%), Space: 43.4 MB (41.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,10 +3,17 @@ class Solution {
         StringBuilder str = new StringBuilder(s.trim().toLowerCase());
         String validSymbols = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+        for (int a = 0; a < str.length(); ) {
+            if (!validSymbols.contains(String.valueOf(str.charAt(a)))) {
+                str.deleteCharAt(a);
+            } else {
+                a++;
+            }
+        }
+
         if (str.length() < 2) {
             return true;
         }
-
 
         int i = 0;
         int j = str.length() - 1;
