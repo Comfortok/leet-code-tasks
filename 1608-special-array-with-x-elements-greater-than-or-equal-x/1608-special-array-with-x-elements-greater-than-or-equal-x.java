4915e33307e9bb297775736f6661be8c96fd0fa3
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-14 18:01:22 +0600

    Time: 1 ms (75.02%), Space: 40.3 MB (32.67%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int x = 0;
+        while (x <= nums.length) {
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] >= x) {
+                    count++;
+                }
+            }
+            
+            if (x == count) {
+                return x;
+            } 
+            
+            x++;
+        }
+        
+        return -1;
+    }
+}
