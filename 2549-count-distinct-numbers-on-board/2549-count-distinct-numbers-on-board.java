f305c31500efc32171891455dd5a34f0176aaaa9
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-15 17:50:00 +0600

    Time: 0 ms (100.00%), Space: 39 MB (81.93%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
@@ -1,19 +1,7 @@
 class Solution {
     
     public int distinctIntegers(int n) {
-        Set<Integer> numsOnBoard = new HashSet<>();
-        numsOnBoard.add(n);
-        countRemainingNums(n, numsOnBoard);
-        return numsOnBoard.size();
-    }
-    
-    private void countRemainingNums(int n, Set<Integer> numsOnBoard) {
-        for (int i = 2; i < n; i++) {
-            if (n % i == 1) {
-                numsOnBoard.add(i);
-                countRemainingNums(i, numsOnBoard);
-            }
-        }
+        return Math.max(n - 1, 1);
     }
     
 }
