87d43f09560c0474e262c36fe8522d7d80a13271
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-15 17:51:27 +0600

    Time: 0 ms (100.00%), Space: 39.1 MB (68.48%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
@@ -1,8 +1,7 @@
 class Solution {
     
     public int distinctIntegers(int n) {
-        return (n - 1 > 1) ? n - 1 : 1;
-        // return Math.max(n - 1, 1);
+        return Math.max(n - 1, 1);
     }
     
 }
