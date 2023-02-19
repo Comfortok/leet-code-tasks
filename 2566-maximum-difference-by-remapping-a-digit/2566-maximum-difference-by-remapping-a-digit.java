8e7ce62d548562a4bc5612efa3abe8c64cd998b2
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-19 21:21:28 +0600

    Time: 5 ms (33.33%), Space: 39.6 MB (16.67%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
@@ -1,14 +1,25 @@
 class Solution {
     public int minMaxDifference(int num) {
         String str = String.valueOf(num);
-        String max = str;
+        int max = num;
+        int multiplier = str.length() - 1;
+        int first = -1;
+        boolean firstFound = false;
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != '9') {
-                max = str.replaceAll(String.valueOf(str.charAt(i)), "9");
-                break;
+            int a = str.charAt(i) - '0';
+            if (a != 9 && !firstFound) {
+                first = a;
+                firstFound = true;
             }
+
+            if (first == a) {
+                int b = 9 - first;
+                max += b * Math.pow(10, multiplier);
+            }
+            multiplier--;
         }
         String min = str.replaceAll(String.valueOf(str.charAt(0)), "0");
-        return Integer.parseInt(max) - Integer.parseInt(min);
+
+        return max - Integer.parseInt(min);
     }
 }
