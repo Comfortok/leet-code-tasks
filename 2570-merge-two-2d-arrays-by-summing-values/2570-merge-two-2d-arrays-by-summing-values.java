c11364442f8b5742ebcc0cf71d7fddf9c1c8b1a6
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-23 21:40:29 +0600

    Time: 1 ms (93.50%), Space: 50.1 MB (12.55%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -1,21 +1,46 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int[] ints : nums1) {
-            map.put(ints[0], ints[1]);
-        }
-
-        for (int[] ints : nums2) {
-            map.put(ints[0], map.getOrDefault(ints[0], 0) + ints[1]);
-        }
-
+        int[][] arr = new int[nums1.length + nums2.length][2];
         int a = 0;
-        int[][] result = new int[map.size()][2];
+        int b = 0;
+        int length = nums1.length + nums2.length;
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            result[a][0] = entry.getKey();
-            result[a][1] = entry.getValue();
-            a++;
+        for (int i = 0; i < length; i++) {
+            if (a == nums1.length) {
+                while (i < length) {
+                    arr[i] = nums2[b];
+                    b++;
+                    i++;
+                }
+                break;
+            } else if (b == nums2.length) {
+                while (i < length) {
+                    arr[i] = nums1[a];
+                    a++;
+                    i++;
+                }
+                break;
+            } else {
+                if (nums1[a][0] < nums2[b][0]) {
+                    arr[i] = nums1[a];
+                    a++;
+                } else if (nums1[a][0] > nums2[b][0]) {
+                    arr[i] = nums2[b];
+                    b++;
+                } else {
+                    arr[i][0] = nums1[a][0];
+                    arr[i][1] = nums1[a][1] + nums2[b][1];
+                    a++;
+                    b++;
+                    length--;
+                }
+            }
+        }
+
+        int[][] result = new int[length][2];
+        for (int i = 0; i < length; i++) {
+            result[i][0] = arr[i][0];
+            result[i][1] = arr[i][1];
         }
 
         return result;
