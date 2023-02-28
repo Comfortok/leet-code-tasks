879b0fbca7e605d3eea8713855e01f9a414f65d4
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-28 10:30:21 +0600

    Time: 8 ms (65.97%), Space: 42.6 MB (86.71%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,20 +1,33 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        int temp = 0;
-        String temp1 = "";
-        for (int i = 0; i < heights.length - 1; i++) {
-            for (int j = i + 1; j < heights.length; j++) {
-                if (heights[i] < heights[j]) {
-                    temp = heights[i];
-                    heights[i] = heights[j];
-                    heights[j] = temp;
-                    temp1 = names[i];
-                    names[i] = names[j];
-                    names[j] = temp1;
-                }
-            }
+        // int temp = 0;
+        // String temp1 = "";
+        // for (int i = 0; i < heights.length - 1; i++) {
+            // for (int j = i + 1; j < heights.length; j++) {
+                // if (heights[i] < heights[j]) {
+                    // temp = heights[i];
+                    // heights[i] = heights[j];
+                    // heights[j] = temp;
+                    // temp1 = names[i];
+                    // names[i] = names[j];
+                    // names[j] = temp1;
+                // }
+            // }
+        // }
+        
+        String[] result = new String[names.length];
+        Map<Integer, String> map = new TreeMap<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
         }
         
-        return names;
+        int i = 0;
+        for(Map.Entry<Integer, String> entry : map.entrySet()) {
+            result[i] = entry.getValue();
+            i++;
+        }
+        
+        return result;
     }
 }
