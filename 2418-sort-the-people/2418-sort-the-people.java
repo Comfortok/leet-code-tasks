1aab7b38749c96915348c18e8d1189348d8714bb
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-02-28 10:30:37 +0600

    Time: 9 ms (58.38%), Space: 43.8 MB (17.93%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,20 +1,5 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        // int temp = 0;
-        // String temp1 = "";
-        // for (int i = 0; i < heights.length - 1; i++) {
-            // for (int j = i + 1; j < heights.length; j++) {
-                // if (heights[i] < heights[j]) {
-                    // temp = heights[i];
-                    // heights[i] = heights[j];
-                    // heights[j] = temp;
-                    // temp1 = names[i];
-                    // names[i] = names[j];
-                    // names[j] = temp1;
-                // }
-            // }
-        // }
-        
         String[] result = new String[names.length];
         Map<Integer, String> map = new TreeMap<>(Collections.reverseOrder());
         
