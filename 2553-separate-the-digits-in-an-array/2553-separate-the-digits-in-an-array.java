4dfb28d50312ad336547e6e9b1b582720dd33866
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-03-07 10:03:21 +0600

    Time: 4 ms (85.58%), Space: 43.4 MB (9.06%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -1,5 +1,20 @@
 class Solution {
     public int[] separateDigits(int[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int num: nums) {
+            sb.append(num);
+        }
+        
+        int[] ans = new int[sb.length()];
+        for (int i = 0; i < sb.length(); i++) {
+            ans[i] = sb.charAt(i) - '0';
+        }
+        
+        return ans;
+        
+    }
+    
+    public int[] separateDigits2(int[] nums) {
         List<Integer> list = new ArrayList<>();
         for (int num : nums) {
             if (num < 10) {
@@ -12,8 +27,7 @@ class Solution {
             }
         }
         
-        return list.stream().mapToInt(i -> i).toArray();
-        
+        return list.stream().mapToInt(i -> i).toArray(); 
     }
     
 }
