33f6fe64554c47a297edc7565669e2ed9a76dfbb
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-03-08 21:54:38 +0600

    Time: 4 ms (18.32%), Space: 42.4 MB (33.84%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
@@ -1,18 +1,14 @@
 class Solution {
     public double trimMean(int[] arr) {
-        List<Integer> list = Arrays
-                .stream(arr)
-                .boxed()
-                .sorted()
-                .collect(Collectors.toList());
-        int fivePercent = (5 * list.size()) / 100;
+        Arrays.sort(arr);
+        int fivePercent = (5 * arr.length) / 100;
 
         int sum = 0;
-        for (int i = fivePercent; i < list.size() - fivePercent; i++) {
-            sum += list.get(i);
+        for (int i = fivePercent; i < arr.length - fivePercent; i++) {
+            sum += arr[i];
         }
 
-        double size = list.size() - (fivePercent * 2);
+        double size = arr.length - (fivePercent * 2);
 
         int multiplier = (int) Math.pow(10, 5);
         double result = sum / size;
