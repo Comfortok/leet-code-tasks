e4600416d4cb808dbf4e5dc4e6855bb06ec24351
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-03-10 13:39:02 +0600

    Time: 56 ms (5.35%), Space: 68.3 MB (5.35%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -47,4 +47,16 @@ class Solution {
 
         return result;
     }
+    
+    public List<List<Integer>> shiftGrid2(int[][] g, int k) {
+        int m = g.length, n = g[0].length, tot = m * n;
+        List<List<Integer>> res= new ArrayList<>();
+        k = k % tot;
+        for (int i = 0; i < m; i++) res.add(new ArrayList<>());
+        for (int l = 0; l < tot; l++) {
+            int idx = (l - k + tot) % tot;
+            res.get(l / n).add(g[idx / n][idx % n]);
+        }
+        return res;
+    }
 }
