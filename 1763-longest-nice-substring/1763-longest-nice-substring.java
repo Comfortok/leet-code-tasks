a5157bbfa9e081c4766b255871aa10fbe11d8d50
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-03-16 15:53:35 +0600

    Time: 45 ms (13.84%), Space: 43.2 MB (8.84%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.java
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -1,30 +1,21 @@
 class Solution {
     public String longestNiceSubstring(String s) {
-        List<String> result = new ArrayList<>();
+        String result = "";
         for (int i = 0; i < s.length() - 1; i++) {
             int start = i;
             int end = s.length();
             StringBuilder sub = new StringBuilder(s.substring(start, end));
             while (sub.length() > 1) {
                 if (isNiceString(sub.toString())) {
-                    result.add(sub.toString());
+                    if (sub.toString().length() > result.length()) {
+                        result = sub.toString();
+                    }
                 }
                 sub.deleteCharAt(sub.length() - 1);
             }
         }
 
-        if (result.isEmpty()) {
-            return "";
-        } else {
-            String first = result.get(0);
-            for (String str : result) {
-                if (str.length() > first.length()) {
-                    first = str;
-                }
-            }
-
-            return first;
-        }
+        return result;
     }
     
     private boolean isNiceString(String s) {
