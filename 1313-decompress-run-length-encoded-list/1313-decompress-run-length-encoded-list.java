797ebff7706cfe751b1d80f14c6ee761718f1fb6
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-03-17 12:36:41 +0600

    Time: 1 ms (88.20%), Space: 43 MB (59.27%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -1,16 +1,21 @@
 class Solution {
     public int[] decompressRLElist(int[] nums) {
-        List<Integer> result = new ArrayList<>();
+        int size = 0;
+        int pos = 0;
+        
+        for (int i = 0; i < nums.length - 1; i = i + 2) {
+            size += nums[i];
+        }
+        
+        int[] result = new int[size];
+        
         for (int i = 0; i < nums.length - 1; i = i + 2) {
             for (int j = 0; j < nums[i]; j++) {
-                result.add(nums[i + 1]);
+                result[pos] = nums[i + 1];
+                pos++;
             }
         }
         
-        int[] arr = new int[result.size()];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = result.get(i);
-        }
-        return arr;
+        return result;
     }
 }
