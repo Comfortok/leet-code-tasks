04bc80e520a0907c0a2aeafec5d3380cbcfa9265
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-04-03 00:29:21 +0600

    Time: 1 ms (55.22%), Space: 40.3 MB (60.27%) - LeetHub

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,14 +1,30 @@
 class Solution {
     public int[] getNoZeroIntegers(int n) {
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; ) {
             if (!String.valueOf(i).contains("0")) {
                 int a = n - i;
                 if (!String.valueOf(a).contains("0")) {
                     return new int[]{i, a};
                 }
             }
+            
+            i = increment(i);
         }
         
         return new int[]{};
+    }
+    
+    private int increment(int i) {
+        if (i % 100 == 0) {
+            i += 11;
+        } else if (i % 1000 == 0) {
+            i += 111;
+        } else if (i % 10000 == 0) {
+            i += 1111;
+        } else {
+            i++;
+        }
+        
+        return i;
     }
 }
