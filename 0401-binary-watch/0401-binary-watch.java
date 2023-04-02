f08ca6ea6e5061f505fa9f23a0b6e92aaad3f1c1
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-04-03 00:11:48 +0600

    Time: 13 ms (30.40%), Space: 43 MB (9.92%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.java
+++ 0401-binary-watch/0401-binary-watch.java
@@ -1,10 +1,21 @@
 class Solution {
     public List<String> readBinaryWatch(int turnedOn) {
-        List<String> times = new ArrayList<>();
-        for (int h = 0; h < 12; h++)
-            for (int m = 0; m < 60; m++)
-                if (Integer.bitCount(h * 64 + m) == turnedOn)
-                    times.add(String.format("%d:%02d", h, m));
-        return times;  
+        List<String> result = new ArrayList<>();
+        if (turnedOn >= 9) {
+            return result;
+        } else if (turnedOn == 0) {
+            result.add(String.format("%d:%02d", 0, 0));
+            return result;
+        }
+        
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    result.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        
+        return result;
     }
 }
