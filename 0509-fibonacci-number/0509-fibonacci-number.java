d53cbaff8ec5b7c3e9a0a95ef1f36b9a4920a67f
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-04-13 22:26:07 +0600

    Time: 0 ms (100.00%), Space: 39.2 MB (67.79%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            if (i < 2) {
+                list.add(i);
+            } else {
+                list.add(list.get(i - 1) + list.get(i - 2));
+            }
+        }
+        
+        return list.get(n);
+    }
+}
