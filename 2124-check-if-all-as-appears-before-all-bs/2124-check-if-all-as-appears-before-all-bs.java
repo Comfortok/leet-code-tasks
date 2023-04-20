22b70eae66b33a451d2ac1ea41ff5596badb0975
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-04-20 13:09:26 +0600

    Time: 0 ms (100.00%), Space: 40.7 MB (53.19%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean checkString(String s) {
-        if (s.lastIndexOf("ba") != -1) {
+        if (s.lastIndexOf("a") < s.indexOf("b") || s.indexOf("b") == -1) {
+            return true;
+        } else {
             return false;
         }
-        
-        return true;
     }
 }
