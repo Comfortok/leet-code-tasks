7777ecd941486310128c9c69e8e95a3b53a9ca7c
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-04-27 11:02:18 +0600

    Time: 1 ms (98.92%), Space: 43.1 MB (10.85%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,18 +1,15 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        int[] result = new int[prices.length];
         for (int i = 0; i < prices.length; i++) {
             int j = i + 1;
             while (j < prices.length && prices[i] < prices[j]) {
                 j++;
             }
             if (j < prices.length) {
-                result[i] = prices[i] - prices[j];
-            } else {
-                result[i] = prices[i];
+                prices[i] = prices[i] - prices[j];
             }
         }
         
-        return result;
+        return prices;
     }
 }
