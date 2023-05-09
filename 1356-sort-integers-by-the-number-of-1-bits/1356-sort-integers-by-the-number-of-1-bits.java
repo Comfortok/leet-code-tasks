9bf54270a74a94ddca970bca6cf39db311df88d4
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-09 19:27:26 +0600

    Time: 48 ms (5.43%), Space: 43 MB (48.85%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,20 +1,13 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        int[] bits = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            bits[i] = Integer.bitCount(arr[i]);
-        }
-        int tempBit = -1;
-        int tempNum = -1;
+        int temp = -1;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
-                if ((bits[j - 1] > bits[j]) || (bits[j - 1] == bits[j] && arr[j - 1] > arr[j])) {
-                    tempBit = bits[j - 1];
-                    tempNum = arr[j - 1];
-                    bits[j - 1] = bits[j];
+                if ((Integer.bitCount(arr[j - 1]) > Integer.bitCount(arr[j])) 
+                    || (Integer.bitCount(arr[j - 1]) == Integer.bitCount(arr[j]) && arr[j - 1] > arr[j])) {
+                    temp = arr[j - 1];
                     arr[j - 1] = arr[j];
-                    arr[j] = tempNum;
-                    bits[j] = tempBit;
+                    arr[j] = temp;
                 }
             }
         }
