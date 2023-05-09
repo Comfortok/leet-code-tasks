af159fcc370aaec8ee26f7eb1ba7bdb67e61eb5e
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-09 19:32:18 +0600

    Time: 2 ms (100.00%), Space: 43 MB (60.51%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        int temp = -1;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j < arr.length - i; j++) {
-                int a = Integer.bitCount(arr[j - 1]);
-                int b = Integer.bitCount(arr[j]);
-                if ((a > b) || (a == b && arr[j - 1] > arr[j])) {
-                    temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
-                }
-            }
+            arr[i] += Integer.bitCount(arr[i]) * 10001;
+        }
+        
+        Arrays.sort(arr);
+        
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] % 10001;
         }
         
         return arr;
