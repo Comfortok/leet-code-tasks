51e23aec606b3934f512ead68934a1f7029fcbfc
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-10 12:41:28 +0600

    Time: 0 ms (100.00%), Space: 40.6 MB (63.83%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,10 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(String.valueOf(s.charAt(i)).toLowerCase());
-        }
-        
-        return sb.toString();
+        return s.toLowerCase();
     }
 }
