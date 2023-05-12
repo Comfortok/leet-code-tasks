82a5e9fee759b6c249e314591aebbe0f2fc10168
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-12 12:55:58 +0600

    Time: 1 ms (98.25%), Space: 39.9 MB (84.30%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,26 +1,18 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        if (stones.length == 1) {
-            return stones[0];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone : stones) {
+            pq.add(stone);
         }
         
-        Arrays.sort(stones);
-        for (int i = stones.length - 1; i > 0; ) {
-            if (stones[i] == -1) {
-                return 0;
-            } else if(stones[i - 1] == -1) {
-                return stones[i];
+        while(pq.size() > 1) {
+            int stone1 = pq.poll();
+            int stone2 = pq.poll();
+            if (stone1 > stone2) {
+                pq.add(stone1-stone2);
             }
-            
-            if (stones[i] - stones[i - 1] == 0) {
-                stones[i - 1] = -1;
-            } else {
-                stones[i - 1] = stones[i] - stones[i - 1];
-            }
-            stones[i] = -1;
-            Arrays.sort(stones);
         }
         
-        return 0;
+        return pq.isEmpty() ? 0 : pq.peek();
     }
 }
