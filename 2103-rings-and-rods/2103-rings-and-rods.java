5732ea91d4ccc2da321a785e3c6c3c3b11a41f47
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-14 12:55:39 +0600

    Time: 3 ms (18.93%), Space: 40.8 MB (25.71%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -2,7 +2,7 @@ class Solution {
     public int countPoints(String rings) {
         int result = 0;
         for (int i = 0; i < 10; i++) {
-            if (rings.contains("R" + i) && rings.contains("G" + i) && rings.contains("B" + i)) {
+            if (rings.indexOf("R" + i) > -1 && rings.indexOf("G" + i) > -1 && rings.indexOf("B" + i) > -1) {
                 result++;
             }
         }
