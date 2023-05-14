ead63789824a47e8ece09c126d54427d4949ee01
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-14 12:59:48 +0600

    Time: 1 ms (71.29%), Space: 40.5 MB (73.50%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,10 +1,22 @@
 class Solution {
     public int countPoints(String rings) {
+        Set<Integer> r = new HashSet<>();
+        Set<Integer> g = new HashSet<>();
+        Set<Integer> b = new HashSet<>();
+        for (int i = 0; i < rings.length(); i += 2) {
+            if (rings.charAt(i) == 'R') {
+                r.add(rings.charAt(i + 1) - '0');
+            } else if (rings.charAt(i) == 'B') {
+                b.add(rings.charAt(i + 1) - '0');
+            } else {
+                g.add(rings.charAt(i + 1) - '0');
+            }
+        }
+        
         int result = 0;
         for (int i = 0; i < 10; i++) {
-            if (rings.indexOf("R" + i) > -1 && rings.indexOf("G" + i) > -1 && rings.indexOf("B" + i) > -1) {
+            if (r.contains(i) && g.contains(i) && b.contains(i))
                 result++;
-            }
         }
         
         return result;
