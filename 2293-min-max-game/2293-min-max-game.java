e9344ebe6b9986e9cb1ecd67175d9fcf7706d4d1
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-15 10:49:26 +0600

    Time: 0 ms (100.00%), Space: 42 MB (80.68%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -1,17 +1,20 @@
 class Solution {
-    public int minMaxGame(int[] nums) {        
-        while (nums.length > 1) {
-            int[] arr = new int[nums.length / 2];
-            for (int i = 0; i < arr.length; i++) {
-                if (i % 2 == 0) {
-                    arr[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
-                } else {
-                    arr[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
-                }
-            }
-            nums = arr;
+    public int minMaxGame(int[] nums) {
+        if (nums.length < 2) {
+            return nums[0];
         }
         
-        return nums[0];
+        int[] arr = new int[nums.length / 2];
+        int index = 0;    
+        for (int i = 1; i < nums.length; i += 2) {
+            if (index % 2 == 0) {
+                arr[index] = Math.min(nums[i], nums[i - 1]);
+            } else {
+                arr[index] = Math.max(nums[i], nums[i - 1]);
+            }
+            index++;
+        }
+        
+        return minMaxGame(arr);
     }
 }
