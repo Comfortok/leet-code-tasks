e5d1b1eb960f1bc772c73a1ef2799aa8a14171b9
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-15 10:43:05 +0600

    Time: 1 ms (57.44%), Space: 42.9 MB (7.83%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -1,32 +1,17 @@
 class Solution {
-    public int minMaxGame(int[] nums) {
-        if (nums.length < 2) {
-            return nums[0];
-        }
-        
-        Queue<Integer> queue = new LinkedList<>();
-        int count = 2;
-        for (int i = 1; i < nums.length; i += 2) {
-            if (count % 2 == 0) {
-                queue.add(Math.min(nums[i - 1], nums[i]));
-            } else {
-                queue.add(Math.max(nums[i - 1], nums[i]));
+    public int minMaxGame(int[] nums) {        
+        while (nums.length > 1) {
+            int[] arr = new int[nums.length / 2];
+            for (int i = 0; i < arr.length; i++) {
+                if (i % 2 == 0) {
+                    arr[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+                } else {
+                    arr[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+                }
             }
-            count++;
-        }
-        count = 2;
-        
-        while (queue.size() > 1) {
-            int a = queue.poll();
-            int b = queue.poll();
-            if (count % 2 == 0) {
-                queue.add(Math.min(a, b));
-            } else {
-                queue.add(Math.max(a, b));
-            }
-            count++;
+            nums = arr;
         }
         
-        return queue.poll();
+        return nums[0];
     }
 }
