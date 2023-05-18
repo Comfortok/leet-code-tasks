c218d9802e020ce5d47bc37785756165e5170572
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-18 09:24:39 +0600

    Time: 1 ms (99.74%), Space: 40.6 MB (63.94%) - LeetHub

--- 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
@@ -2,19 +2,19 @@ class Solution {
     public int minNumber(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int start1 = 0;
-        int start2 = 0;
-        while (start1 < nums1.length && start2 < nums2.length) {
-            if (nums1[start1] == nums2[start2]) {
-                return nums1[start1];
-            } else if (nums1[start1] > nums2[start2]) {
-                start2++;
-            } else {
-                start1++;
+
+        for (int i = 0; i < nums1.length; i++) {
+            for(int j = 0; j < nums2.length; j++) {
+                if(nums1[i] == nums2[j]) {
+                    return nums1[i];
+                }
             }
         }
         
-        String result = nums1[0] < nums2[0] ? "" + nums1[0] + nums2[0] : "" + nums2[0] + nums1[0];
-        return Integer.valueOf(result);
+        if (nums1[0] < nums2[0]) {
+            return (nums1[0] * 10) + nums2[0];
+        }
+        
+        return (nums2[0] * 10) + nums1[0];
     }
 }
