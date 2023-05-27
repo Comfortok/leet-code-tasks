acf9e3f3e9628155b5b08aa43c80cb1ce8f17637
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-05-27 20:09:35 +0600

    Time: 2 ms (86.82%), Space: 41.2 MB (60.24%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean areOccurrencesEqual(String s) {
         int[] table = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            table[ch - 'a']++;
+            table[s.charAt(i) - 'a']++;
         }
         
         int count = 0;
