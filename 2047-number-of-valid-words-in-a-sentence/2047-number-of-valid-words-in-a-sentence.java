a53f834d197f9be2e37d60f859bb309d1493972a
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-06-02 11:26:38 +0600

    Time: 47 ms (21.05%), Space: 44.6 MB (6.32%) - LeetHub

--- 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.java
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.java
@@ -1,30 +1,7 @@
 class Solution {
     public int countValidWords(String sentence) {
-        String[] words = sentence.strip().split("\\s+");
-        int count = 0;
-        for (String word : words) {
-            if (isValid(word)) {
-                count++;
-            }
-        }
-        return count;
-    }
-    
-    private boolean isValid(String word) {
-        if (((word.lastIndexOf('!') != -1) && (word.lastIndexOf('!') != word.indexOf('!') || word.indexOf('!') != word.length() - 1))
-                || ((word.lastIndexOf('.') != -1) && (word.lastIndexOf('.') != word.indexOf('.') || word.indexOf('.') != word.length() - 1))
-                || ((word.lastIndexOf(',') != -1) && (word.lastIndexOf(',') != word.indexOf(',') || word.indexOf(',') != word.length() - 1))
-                || ((word.lastIndexOf('-') != -1) && (word.lastIndexOf('-') != word.indexOf('-') || word.indexOf('-') == 0 || word.indexOf('-') == word.length() - 1
-                || !Character.isAlphabetic(word.charAt(word.indexOf('-') + 1))))) {
-            return false;
-        } else {
-            for (int i = 0; i < word.length(); i++) {
-                if (Character.isDigit(word.charAt(i))) {
-                    return false;
-                }
-            }
-        }
-        
-        return true;
+        String[] words = sentence.trim().split("\\s+");
+        String regex = "([a-z]+(-?[a-z]+)?)?[!.,]?";
+        return (int) Arrays.stream(words).filter(w -> w.matches(regex)).count();
     }
 }
