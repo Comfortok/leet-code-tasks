46e966417f27e1949fda41c4f644013bcc194116
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-06-08 11:36:31 +0600

    Time: 0 ms (100.00%), Space: 40.4 MB (85.03%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -2,14 +2,18 @@ class Solution {
     public int maxDistance(int[] colors) {
         int max = 0;
         for (int i = 0; i < colors.length - 1; i++) {
-            for (int j = colors.length - 1; j > i; j--) {
-                if (j - i > max) {
-                    if (colors[i] != colors[j]) {
-                        max = j - i; 
+            if (colors.length - 1 - i > max) {
+                for (int j = colors.length - 1; j > i; j--) {
+                    if (j - i > max) {
+                        if (colors[i] != colors[j]) {
+                            max = j - i; 
+                        }
+                    } else {
+                        break;
                     }
-                } else {
-                    break;
                 }
+            } else {
+                break;
             }
         }
         
