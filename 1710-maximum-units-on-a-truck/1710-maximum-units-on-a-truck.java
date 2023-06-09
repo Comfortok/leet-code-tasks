fd606919e3c8202a2ed65556bb976e98d887194e
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-06-09 12:02:12 +0600

    Time: 28 ms (13.59%), Space: 43.8 MB (33.82%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,6 +1,9 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        for (int i = 0; i < boxTypes.length - 1; i++) {
+        int boxes = 0;
+        int size = 0;
+
+        for (int i = 0; i < boxTypes.length; i++) {
             int maxIndex = i;
             for (int j = i + 1; j < boxTypes.length; j++) {
                 if (boxTypes[maxIndex][1] < boxTypes[j][1]) {
@@ -8,27 +11,16 @@ class Solution {
                 }
             }
 
-            if (i != maxIndex) {
+            if (size + boxTypes[maxIndex][0] <= truckSize) {
+                boxes += boxTypes[maxIndex][0] * boxTypes[maxIndex][1];
+                size += boxTypes[maxIndex][0];
                 int[] temp = boxTypes[i];
                 boxTypes[i] = boxTypes[maxIndex];
                 boxTypes[maxIndex] = temp;
-            }
-        }
-
-        int boxes = 0;
-        int size = 0;
-        for (int i = 0; i < boxTypes.length; i++) {
-            if (size > truckSize) {
+            } else {
+                boxes += boxTypes[maxIndex][1] * (truckSize - size);
                 break;
             }
-
-            if (size + boxTypes[i][0] <= truckSize) {
-                boxes += boxTypes[i][0] * boxTypes[i][1];
-            } else {
-                boxes += boxTypes[i][1] * (truckSize - size);
-            }
-
-            size += boxTypes[i][0];
         }
 
         return boxes;
