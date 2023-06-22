e1a4ce8c4fd87f5aa1f2804398f4a81a35afd02a
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-06-22 10:41:34 +0600

    Time: 25 ms (96.58%), Space: 50.5 MB (46.87%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,26 +1,18 @@
 class RecentCounter {
 
-    List<Integer> requests;
+    private LinkedList<Integer> requests;
     
     public RecentCounter() {
-        this.requests = new ArrayList<>();
+        this.requests = new LinkedList<>();
     }
     
     public int ping(int t) {
-        int count = 1;
-        if (!requests.isEmpty()) {
-            for (int i = requests.size() - 1; i >= 0; i--) {
-                if (t - 3000 <= requests.get(i) && requests.get(i) <= t) {
-                    count++;
-                } else {
-                    break;
-                }
-            }
+        requests.addLast(t);
+        while (t - 3000 > requests.getFirst()) {
+            requests.removeFirst();
         }
         
-        requests.add(t);
-        
-        return count;
+        return requests.size();
     }
 }
 
