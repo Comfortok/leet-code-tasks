2c9045041736c70ec50156ca985bbd5851a5e395
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-06-27 12:44:38 +0600

    Time: 0 ms (100.00%), Space: 39.6 MB (96.29%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,6 +1,6 @@
 class Solution {
     public String convertToBase7(int num) {
         if (num == 0) return "0";
-        return Integer.toString(Integer.parseInt(String.valueOf(num), 10), 7);
+        return Integer.toString(num, 7);
     }
 }
