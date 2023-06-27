e4e8fc28caf19597f81686461049a2494ca43301
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-06-27 12:43:09 +0600

    Time: 1 ms (67.66%), Space: 39.8 MB (92.46%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,21 +1,6 @@
 class Solution {
     public String convertToBase7(int num) {
         if (num == 0) return "0";
-        String result = "";
-        int remainder;
-        boolean isNegative = (num < 0) ? true : false;
-        if (isNegative) num *= -1;
-        
-        while (num > 0) {
-            remainder = num % 7;
-            result += remainder;
-            num /= 7;
-        }
-        
-        if (isNegative) {
-            return new StringBuffer(result).append("-").reverse().toString();
-        } else {
-            return new StringBuffer(result).reverse().toString();
-        }
+        return Integer.toString(Integer.parseInt(String.valueOf(num), 10), 7);
     }
 }
