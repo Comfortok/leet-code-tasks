35fbd6c5326963564f3f5b9b16bbbcc34fd2f1f6
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-03 10:17:52 +0600

    Time: 1 ms (75.45%), Space: 40.6 MB (56.68%) - LeetHub

--- 2299-strong-password-checker-ii/2299-strong-password-checker-ii.java
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.java
@@ -1,32 +1,21 @@
 class Solution {
     public boolean strongPasswordCheckerII(String password) {
-        if (password.length() < 8) {
-            return false;
-        }
+        int l = password.length();
+        if (l < 8) return false;
         
         String specials = "!@#$%^&*()-+";
- 
         boolean hasLowercase = false;
         boolean hasUppercase = false;
         boolean hasSpecialChar = false;
         boolean hasDigit = false;
-        for (int i = 0; i < password.length(); i++) {
+        
+        for (int i = 0; i < l; i++) {
             char ch = password.charAt(i);
-            if (i != 0 && password.charAt(i - 1) == ch) {
-                return false;
-            }
-            if (!hasLowercase && ch >= 'a' && ch <= 'z') {
-                hasLowercase = true;
-            }
-            if (!hasUppercase  && ch >= 'A' && ch <= 'Z') {
-                hasUppercase = true;
-            }
-            if (!hasSpecialChar && specials.contains(String.valueOf(ch))) {
-                hasSpecialChar = true;
-            }
-            if (!hasDigit && ch >= '0' && ch <= '9') {
-                hasDigit = true;
-            }
+            if (i > 0 && password.charAt(i - 1) == ch) return false;
+            if (!hasLowercase) hasLowercase = ch >= 'a' && ch <= 'z';
+            if (!hasUppercase) hasUppercase = ch >= 'A' && ch <= 'Z';
+            if (!hasSpecialChar) hasSpecialChar = specials.contains(String.valueOf(ch));
+            if (!hasDigit) hasDigit = ch >= '0' && ch <= '9';
         }
         
         return hasLowercase && hasUppercase && hasSpecialChar && hasDigit;
