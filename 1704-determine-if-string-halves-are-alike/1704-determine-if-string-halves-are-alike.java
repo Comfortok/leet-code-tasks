88f6413b7699ff9ccac24eafa7ae990e5f0ab5cb
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-11 13:29:06 +0600

    Time: 4 ms (66.02%), Space: 41.1 MB (43.66%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -4,17 +4,16 @@ class Solution {
         int half = s.length() / 2;
         String a = s.substring(0, half);
         String b = s.substring(half, s.length());
-        int ac = 0;
-        int bc = 0;
+        int count = 0;
         for (int i = 0; i < a.length(); i++) {
             if (vowels.indexOf(a.charAt(i)) != -1) {
-                ac++;
+                count++;
             }
             if (vowels.indexOf(b.charAt(i)) != -1) {
-                bc++;
+                count--;
             }
         }
         
-        return ac == bc;
+        return count == 0;
     }
 }
