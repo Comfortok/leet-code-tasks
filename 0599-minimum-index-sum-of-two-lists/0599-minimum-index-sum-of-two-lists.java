60e4fd8c0e5f75e52043117363d5b85cd8196c22
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-13 13:40:37 +0600

    Time: 9 ms (83.99%), Space: 44.6 MB (55.80%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
@@ -1,29 +1,24 @@
 class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        int minIndex = -1;
-        List<Integer> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < list1.length; i++) {
-            for (int j = 0; j < list2.length; j++) {
-                if (list1[i].equals(list2[j])) {
-                    if (minIndex == -1) {
-                        minIndex = i + j;
-                        list.add(i);
-                    } else if (minIndex > i + j) {
-                        minIndex = i + j;
-                        list.clear();
-                        list.add(i);
-                    } else if (minIndex == i + j) {
-                        list.add(i);
-                    }
-                }
-            }
+            map.put(list1[i], i);
         }
         
-        String[] result = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list1[list.get(i)];
+        int min = 2001;
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < list2.length; i++) {
+            int idx = map.getOrDefault(list2[i], 2001);
+            int sum = idx + i;
+            if (min > sum) min = sum;
         }
-        
-        return result;
+
+        for (int i = 0; i < list2.length; i++) {
+            int idx = map.getOrDefault(list2[i], 2001);
+            int sum = idx + i;
+            if (sum == min) words.add(list2[i]);
+        }
+
+        return words.toArray(new String[0]);
     }
 }
