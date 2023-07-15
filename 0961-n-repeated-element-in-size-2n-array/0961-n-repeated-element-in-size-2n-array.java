c274b6a472ebf9d0946cf604cdcf7038832fac54
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-15 12:54:45 +0600

    Time: 0 ms (100.00%), Space: 43.9 MB (82.90%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -2,14 +2,9 @@ class Solution {
     public int repeatedNTimes(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        
-        int n = nums.length / 2;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == n) {
-                return entry.getKey();
-            }
+            int value = map.getOrDefault(num, 0) + 1;
+            if (value > 1) return num;
+            else map.put(num, value);
         }
         
         return -1;
