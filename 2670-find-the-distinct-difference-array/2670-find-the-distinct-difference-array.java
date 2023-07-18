322c3cd4e75bf750898b2495b33916b2cfef6c5e
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-18 18:35:14 +0600

    Time: 15 ms (46.58%), Space: 44.5 MB (72.75%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.java
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] distinctDifferenceArray(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
         Set<Integer> set = new HashSet<>();
+        int[] res = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j <= i; j++) set.add(nums[j]);
             int val = set.size();
@@ -9,13 +9,8 @@ class Solution {
             for (int j = i; j < nums.length; j++) {
                 if (j + 1 < nums.length) set.add(nums[j + 1]);
             }
-            map.put(i, val - set.size());
+            res[i] = val - set.size();
             set.clear();
-        }
-        
-        int[] res = new int[nums.length];
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            res[entry.getKey()] = entry.getValue();
         }
         
         return res;
