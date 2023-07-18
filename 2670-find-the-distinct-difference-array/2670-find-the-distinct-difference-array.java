d005027c07aee5395ce5a15793c39ff2af7b5877
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-18 18:38:04 +0600

    Time: 4 ms (100.00%), Space: 44.7 MB (56.45%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.java
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.java
@@ -1,18 +1,35 @@
 class Solution {
     public int[] distinctDifferenceArray(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int[] res = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j <= i; j++) set.add(nums[j]);
-            int val = set.size();
-            set.clear();
-            for (int j = i; j < nums.length; j++) {
-                if (j + 1 < nums.length) set.add(nums[j + 1]);
+        int n = nums.length;
+        int[] answer = new int[n];
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        boolean[] prefixSeen = new boolean[51];
+        boolean[] suffixSeen = new boolean[51];
+
+        for (int i = 0, j = 1; i < n; i++) {
+            if (!prefixSeen[nums[i]]) {
+                prefixSeen[nums[i]] = true;
+                prefix[i] = j++;
+            } else if (i > 0) {
+                prefix[i] = prefix[i-1];
             }
-            res[i] = val - set.size();
-            set.clear();
         }
-        
-        return res;
+
+        for (int i = n-1, j = 1; i >= 0; i--) {
+            if (!suffixSeen[nums[i]]) {
+                suffixSeen[nums[i]] = true;
+                suffix[i] = j++;
+            } else if (i < n-1) {
+                suffix[i] = suffix[i+1];
+            }
+        }
+
+        for (int i = 0; i < n-1; i++) {
+            answer[i] = prefix[i] - suffix[i+1];
+        }
+        answer[n-1] = prefix[n-1];
+
+        return answer;
     }
 }
