74751d1f9e17b93ef1ac4446cc1e267e324b4dc7
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-22 09:37:31 +0600

    Time: 0 ms (100.00%), Space: 40.9 MB (57.12%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,8 +1,5 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double[] result = new double[2];
-        result[0] = celsius + 273.15;
-        result[1] = (celsius * 1.8) + 32.0;
-        return result;
+        return new double[]{(celsius + 273.15), ((celsius * 1.8) + 32.0)};
     }
 }
