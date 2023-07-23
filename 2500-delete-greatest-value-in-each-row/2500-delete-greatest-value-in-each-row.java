bffaafcba39bfa692bda5f5ec77b4b4b725f1dd0
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-23 22:19:08 +0600

    Time: 3 ms (98.35%), Space: 43 MB (93.19%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
@@ -1,16 +1,8 @@
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
         int result = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int row = 0; row < grid.length; row++) {
-            int[] arr = new int[grid[0].length];
-            int ind = 0;
-            for (int col = 0; col < grid[0].length; col++) {
-                arr[ind] = grid[row][col];
-                ind++;
-            }
+        for (int[] arr : grid) {
             Arrays.sort(arr);
-            grid[row] = arr;
         }
             
         for (int col = 0; col < grid[0].length; col++) {
