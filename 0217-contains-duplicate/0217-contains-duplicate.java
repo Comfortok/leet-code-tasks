37983c8a2c43bb5a028fbaa0f82f396e2cb774b2
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-26 21:01:44 +0600

    Time: 21 ms (33.51%), Space: 57.6 MB (23.34%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            if (map.get(num) != null) {
-                return true;
-            }
-            map.put(num, num);
+        Arrays.sort(nums); 
+        for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i+1]) {
+                    return true;
+                }
         }
-        
         return false;
     }
 }
