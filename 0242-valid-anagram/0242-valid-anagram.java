bee09613c639b3f60a79327c8d6167bd9d67266f
Comfortok <50988152+Comfortok@users.noreply.github.com>
2023-07-26 08:56:19 +0600

    Time: 4 ms (87.05%), Space: 42.3 MB (69.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch1 = s.charAt(i);
-            map.put(ch1, map.getOrDefault(ch1, 0) + 1);
-            char ch2 = t.charAt(i);
-            if (map.containsKey(ch2)) {
-                map.put(ch2, map.get(ch2) - 1);
-            } else {
-                map.put(ch2, -1);
-            }
-        }
-        
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue() != 0) {
-                return false;
-            }
-        }
-        
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
         return true;
     }
 }
